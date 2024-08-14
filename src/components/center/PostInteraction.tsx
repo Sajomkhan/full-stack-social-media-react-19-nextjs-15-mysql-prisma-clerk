27c3f289bbd44c9dda6a8bdc67449cd59d7e7f8e
Saiful Ajom Khan <sajomkhan@gmail.com>
2024-08-14 10:22:29 -0700

    Updated

--- src/components/center/PostInteraction.tsx
+++ src/components/center/PostInteraction.tsx
@@ -1,53 +1,98 @@
-"use client"
+"use client";
 
-import Image from "next/image"
+import { switchLike } from "@/lib/actions";
+import { useAuth } from "@clerk/nextjs";
+import Image from "next/image";
+import { useOptimistic, useState } from "react";
 
-const PostInteraction = ({postId, likes, commentsNumber}:{postId:number, likes:string[], commentsNumber:number}) => {
+const PostInteraction = ({
+  postId,
+  likes,
+  commentsNumber,
+}: {
+  postId: number;
+  likes: string[];
+  commentsNumber: number;
+}) => {
+  const {isLoaded, userId} = useAuth();
+  const [likeState, setLikeState] = useState({
+    likeCount: likes.length,
+    isLiked: userId ? likes.includes(userId) : false,
+  });
+
+  const [optimisticLike, switchOptimisticLike] = useOptimistic(
+    likeState,
+    (state, value) => {
+      return {
+        likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+        isLiked: !state.isLiked,
+      };
+    }
+  );
+
+  const likeAction = async()=> {
+    switchOptimisticLike("")
+    try {
+      switchLike(postId)
+      setLikeState((state)=>({
+        likeCount: state.isLiked ? state.likeCount - 1 : state.likeCount + 1,
+        isLiked: !state.isLiked,
+      }))
+    } catch (err) {
+      
+    }
+  }
+
   return (
     <div className="flex items-center justify-between text-sm my-4">
-    <div className="flex gap-4">
-      <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
-        <Image
-          src="/like.png"
-          alt=""
-          width={16}
-          height={16}
-          className="cursor-pointer"
-        />
-        <span className="text-gray-300">|</span>
-        <span className="text-gray-500">
-          123 <span className="hidden md:inline"> Likes</span>
-        </span>
+      <div className="flex gap-4">
+        <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
+          <form action={likeAction}>
+            <button>
+              <Image
+                src={optimisticLike.isLiked ? "/liked.png" : "/like.png"}
+                alt=""
+                width={16}
+                height={16}
+                className="cursor-pointer"
+              />
+            </button>
+          </form>
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">
+            {optimisticLike.likeCount}{" "}
+            <span className="hidden md:inline"> Likes</span>
+          </span>
+        </div>
+        <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
+          <Image
+            src="/comment.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">
+            24 <span className="hidden md:inline"> Comments</span>
+          </span>
+        </div>
       </div>
-      <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
-        <Image
-          src="/comment.png"
-          alt=""
-          width={16}
-          height={16}
-          className="cursor-pointer"
-        />
-        <span className="text-gray-300">|</span>
-        <span className="text-gray-500">
-          24 <span className="hidden md:inline"> Comments</span>
-        </span>
+      <div className="">
+        <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
+          <Image
+            src="/share.png"
+            alt=""
+            width={16}
+            height={16}
+            className="cursor-pointer"
+          />
+          <span className="text-gray-300">|</span>
+          <span className="text-gray-500">Share</span>
+        </div>
       </div>
     </div>
-    <div className="">
-      <div className="flex items-center gap-2 bg-slate-50 py-1 px-2 rounded-lg">
-        <Image
-          src="/share.png"
-          alt=""
-          width={16}
-          height={16}
-          className="cursor-pointer"
-        />
-        <span className="text-gray-300">|</span>
-        <span className="text-gray-500">Share</span>
-      </div>
-    </div>
-  </div>
-  )
-}
+  );
+};
 
-export default PostInteraction
+export default PostInteraction;
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -216,3 +216,38 @@ export const updateProfile = async (
     return { success: false, error: true };
   }
 };
+
+
+export const switchLike = async(postId: number) => {
+  const {userId} = auth()
+
+  if(!userId) throw new Error("User is not authenticated")
+
+    try {
+      const existingLike = await prisma.like.findFirst({
+        where:{
+          postId,
+          userId
+        }
+      })
+
+      if(existingLike){
+        await prisma.like.delete({
+          where:{
+            id:existingLike.id
+          }
+        })
+      }else{
+        await prisma.like.create({
+          data:{
+            postId,
+            userId
+          }
+        })
+      }
+      
+    } catch (err) {
+      console.log(err);
+      throw new Error("Something went wrong")      
+    }
+}
